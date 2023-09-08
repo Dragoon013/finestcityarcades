0396f0becaaad6ecbb3cfd5f6154de1d64ce44cf
dragoon013 <jacob.a.teal@gmail.com>
2023-09-08 17:47:15 -0600

    fixed some text in about

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,13 +7,13 @@
 	<h1>About the Filthy Flipper</h1>
 
 	<p>
-		The Filthy Flipper is a a dream come true. Born out of a love for all things pinball and a serious
-		deficit of pinball machines in San North and East San Diego county, The Filthy Flipper aims to 
+		The Filthy Flipper is a dream come true. Born out of a love for all things pinball and a serious
+		deficit of pinball machines in North and East San Diego county, The Filthy Flipper aims to 
 		provide businesses with the means to give players another reason to visit their establishment.
 	</p>
 
 	<p>
-		We are happy to provide pinball machines for short-long term engagements. Please reach out directly
+		We are happy to provide pinball machines for short and long term engagements. Please reach out directly
 		if you'd like to discuss hosting machines onsite for extended periods of time.
 	</p>
 </div>
