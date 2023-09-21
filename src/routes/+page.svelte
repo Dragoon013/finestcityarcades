248e69a84a4aa8dbcd5a859c471e4ed2dbe68157
Jacob Teal <jacob.a.teal@gmail.com>
2023-09-21 09:57:38 -0400

    getting carousel working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,48 +1,64 @@
 <script>
-	  import Carousel from 'svelte-carousel'
-	  import { browser } from '$app/environment';
-	  import star_wars from '$lib/images/star_wars_flyer.png';
+import Carousel from 'svelte-carousel'
+import {
+    browser
+} from '$app/environment';
+import star_wars from '$lib/images/star_wars_flyer.png';
+import deadpool from '$lib/images/dp_pro_flyer.png';
+import full_house from '$lib/images/full_house_pinball.jpeg';
 
-
-	let carousel; // for calling methods of the carousel instance
-
+let carousel; // for calling methods of the carousel instance
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="home" content="The Friendly Flipper site" />
-</svelte:head>
+    <title>Home</title>
+    <meta name="home" content="The Friendly Flipper site" />
+    </svelte:head>
 
-<section>
-	<h1>
-		Welcome to the Friendly Flipper
-	</h1>
-	{#if browser}
-	<Carousel
-	  bind:this={carousel}
-	>
-	  <div>
-		<picture>
-		<source srcset={star_wars} type="png" />
-		<img src={star_wars} alt="star_wars" />
-	</picture>
-</div>
-	  <div>2</div>
-	  <div>3</div>
-	</Carousel>
-  {/if}
-</section>
+    <section>
+        <h1>
+            Welcome to the Friendly Flipper
+        </h1>
+        {#if browser}
+        <Carousel
+            bind:this={carousel}
+            autoplay
+            >
+            <div class="pinball_slide">
+                <source srcset={deadpool} type="png" />
+                <img src={deadpool} alt="deadpool" class="pinball_image" />
+            </div>
+            <div>
+                <source srcset={star_wars} type="png" />
+                <img src={star_wars} alt="star_wars" class="pinball_image" />
+            </div>
+            <div>
+                <source srcset={full_house} type="jpeg" />
+                <img src={full_house} alt="full_house" class="pinball_image" />
+            </div>
+        </Carousel>
+        {/if}
+    </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+}
 
-	h1 {
-		width: 100%;
-	}
+.pinball_slide {
+    align-items: center;
+}
+
+.pinball_image {
+    height: 500px
+}
+
+
+h1 {
+    width: 100%;
+}
 </style>
