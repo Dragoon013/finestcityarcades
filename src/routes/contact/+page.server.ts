fe0149691ac11b251608a472edb1fc41bdfacc9e
Jacob Teal <jacob.a.teal@gmail.com>
2023-11-13 13:39:23 -0800

    contact us form AWS implementation

--- src/routes/contact/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { sql } from "@vercel/postgres";
-
-export async function actions({ request }) {
-  const formData = Object.fromEntries(await request.formData());
-  return {
-    success:true,
-    formData
-  }
-}
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,27 @@
 <script>
-export let form;
-</script>
+ let name = "";
+  let email = "";
+  let message = "";
+
+  async function handleSubmit() {
+    // You can add validation here
+    // If valid, send the data to the backend
+    const response = await fetch(
+      "https://srdx91a4we.execute-api.us-east-1.amazonaws.com/contact",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, message }),
+      }
+    );
+
+    // Handle the response from the API Gateway (e.g., show a success message)
+    const result = await response.json();
+    console.log(result);
+  }
+  </script>
 
 <svelte:head>
     <title>Contact Us</title>
@@ -10,7 +31,7 @@ export let form;
     <div class="text-column">
         <h1>Contact Us</h1>
 
-        <form class="my-4 " action="?/contactus" method= "POST">
+        <form class="my-4 " on:submit|preventDefault={handleSubmit}>
             <div>
                 <label class="label text-slate-500 mr-4" for="name">Name </label>
                 <input type="text" name="name" placeholder="Wade Wilson" class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full" />
@@ -20,10 +41,6 @@ export let form;
                 <input type="email" name="email" placeholder="wade.wilson@hotmail.com" class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full" />
             </div>
             <div>
-                <label class="label text-slate-500 mr-4" for="name">Venue </label>
-                <input type="text" name="venue" placeholder="Where is the event taking place?" class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full" />
-            </div>
-            <div>
                 <label class="label text-slate-500 mr-4" for="name">Message </label>
                 <textarea class="textarea ring-1 ring-slate-200 focus:outline-none rounded-sm w-full" placeholder="# of machines, special requests, date, etc"></textarea>
             </div>
@@ -31,13 +48,6 @@ export let form;
                 <button type="submit" class="mt-2 w-1/4 btn px-8 rounded-sm btn-outline">Send</button>
             </div>
         </form>
-
-        {#if form?.success }
-        <div class="p-4 bg-slate-100 w-3/6 mt-4">
-            <p>Name: {form?.formData.name}</p>
-            <p>Message: {form?.formData.message}</p>
-        </div>
-        {/if}
 
         <p>
             Reach out to us directly at <a href="mailto:jacob.a.teal@gmail.com">jacob.a.teal@gmail.com</a>.
