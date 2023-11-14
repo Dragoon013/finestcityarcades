9b5ceabb3b1e56122836dd5fa1f44e36f3ec69cd
Jacob Teal <jacob.a.teal@gmail.com>
2023-11-14 10:30:50 -0800

    contact form white text

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -34,15 +34,15 @@
         <form class="my-4 " on:submit|preventDefault={handleSubmit}>
             <div>
                 <label class="label text-slate-500 mr-4" for="name">Name </label>
-                <input type="text" name="name" placeholder="Wade Wilson" class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full" />
+                <input type="text" name="name" placeholder="Wade Wilson" class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full" style="color:white" />
             </div>
             <div>
                 <label class="label text-slate-500 mr-4" for="name">Email </label>
-                <input type="email" name="email" placeholder="wade.wilson@hotmail.com" class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full" />
+                <input type="email" name="email" placeholder="wade.wilson@hotmail.com" class="ring-1 ring-slate-200 focus:outline-none rounded-sm input w-full" style="color:white" />
             </div>
             <div>
                 <label class="label text-slate-500 mr-4" for="name">Message </label>
-                <textarea class="textarea ring-1 ring-slate-200 focus:outline-none rounded-sm w-full" placeholder="# of machines, special requests, date, etc"></textarea>
+                <textarea class="textarea ring-1 ring-slate-200 focus:outline-none rounded-sm w-full" style="color:white" placeholder="# of machines, special requests, date, etc"></textarea>
             </div>
             <div>
                 <button type="submit" class="mt-2 w-1/4 btn px-8 rounded-sm btn-outline">Send</button>
