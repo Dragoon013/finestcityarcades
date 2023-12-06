54edcf0f4fbae02fc37cfd706b3db8fd8f26e34a
Jacob Teal <jacob.a.teal@gmail.com>
2023-12-06 12:17:11 -0800

    changed the machine vixz from a table to cards per machine

--- src/lib/Machines.svelte
+++ src/lib/Machines.svelte
@@ -17,14 +17,8 @@
 		if (!timestamp) return 'never';
 		return `${ms(Date.now() - new Date(timestamp).getTime())}${timeOnly ? '' : ' ago'}`;
 	}
-	function refreshPage() {
-		location.reload();
-	}
 </script>
 
-<div
-	class="w-full max-w-xl p-12 mx-auto rounded-lg shadow-xl dark:bg-white/10 bg-white/30 ring-1 ring-gray-900/5 backdrop-blur-lg"
->
 	<div class="flex items-center justify-between mb-4">
 		<div class="space-y-1">
 			<h2 class="text-xl font-semibold">Machines</h2>
@@ -32,26 +26,26 @@
 				Fetched {machines.length} machines in {duration ? duration : 'unknown'} ms
 			</p>
 		</div>
-		<button on:click={refreshPage}>Refresh Page</button>
 	</div>
-	<div class="divide-y divide-gray-900/5">
+	<div class="flex w-full">
 		{#each machines as machine (machine.id)}
-			<div class="flex items-center justify-between py-3">
-				<div class="flex items-center space-x-4">
-					<img
-						src={machine.image}
-						alt={machine.name}
-						width={48}
-						height={48}
-						class="rounded-full ring-1 ring-gray-900/5"
-					/>
-					<div class="space-y-1">
-						<p class="font-medium leading-none">{machine.name}</p>
-						<p class="text-sm text-gray-500">{machine.status}</p>
-					</div>
-				</div>
-				<p class="text-sm text-gray-500">{timeAgo(machine.createdAt)}</p>
+
+		<div class="card w-96 bg-base-100 shadow-xl">
+			<figure><img src={machine.image} alt={machine.name} /></figure>
+			<div class="card-body">
+			  <h2 class="card-title">{machine.name}</h2>
+			  <p>This is a description about a pinball machine</p>
+			  <div class="card-actions justify-end">
+				<button class="btn btn-primary">Reserve Now</button>
+			  </div>
 			</div>
+		  </div>
+		  <div class="divider divider-horizontal"></div>
 		{/each}
 	</div>
-</div>
+
+	<style>
+	 .card-body{
+		color: white;
+	}
+	</style>
